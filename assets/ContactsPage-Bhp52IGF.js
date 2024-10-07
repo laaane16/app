4762c48eacd6d73a2f8520d49392cf577893c5d1
laaane16 <nikon200402@gmail.com>
2024-10-07 19:52:38 +0300

    Updates

--- assets/ContactsPage-Bhp52IGF.js
+++
@@ -1,1 +0,0 @@
-import{j as t,D as a}from"./index-BjguW4Gv.js";const n=()=>t.jsx("main",{style:{paddingBottom:"100px"},className:"contacts-page app-content",children:t.jsx(a,{})});export{n as default};
